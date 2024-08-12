741c576e2f4c7371544ce0c99938cccefbe7218c
Mayank <vishnu15788@gmail.com>
2024-08-12 09:47:28 +0530

    this AWS buildspec.yam and docker file

--- src/main/java/com/example/demo/MyFirstController.java
+++ src/main/java/com/example/demo/MyFirstController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyFirstController {
     @GetMapping(path = "/getName")
     String getName(){
-        return  "Vishnu Karam chand Gandhi";
+        return  "Vishnu Karam chand Gandhi2";
     }
 
 }
